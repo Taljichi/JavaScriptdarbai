2cfdf4e7b38e053d8650970331e4998de71317b0
Marius Jankauskas <marius40555@gmail.com>
2023-11-23 21:55:31 +0200

    Darbai

--- 05-js-dom/10-projektas/scripts.js
+++ 05-js-dom/10-projektas/scripts.js
@@ -61,3 +61,86 @@ function rodytiReiksme() {
       alert("Įveskite tekstą!");
   }
 }
+
+// =============================================== ASTUNTAS
+
+function pasleptiRodyti() {
+  var tekstasElementas = document.getElementById("tekstas");
+  if (tekstasElementas.style.display === "none" || tekstasElementas.style.display === "") {
+      tekstasElementas.style.display = "block";
+  } else {
+      tekstasElementas.style.display = "none";
+  }
+}
+
+// ================================================= DEVINTAS
+
+var skaitliukoReiksme = 0;
+
+function padidintiSkaitliuka() {
+    skaitliukoReiksme++;
+    document.getElementById("skaitliukas").textContent = skaitliukoReiksme;
+}
+
+// ==================================================== DESIMTAS
+
+function skaiciuotiIlgį() {
+  var tekstas = document.getElementById("tekstoLaukas").value;
+  var ilgis = tekstas.length;
+  document.getElementById("rezultatas").textContent = "Teksto ilgis: " + ilgis;
+}
+
+// ==================================================== VIENUOLIKTAS
+
+var mygtukoNumeris = 1;
+
+function pridetiMygtuka() {
+    var mygtukas = document.createElement("button");
+    mygtukas.textContent = "Mygtukas " + mygtukoNumeris;
+    mygtukas.id = "mygtukas" + mygtukoNumeris;
+    mygtukas.onclick = function() {
+        alert("Paspaustas mygtukas " + mygtukoNumeris);
+    };
+
+    document.getElementById("mygtukuKonteineris").appendChild(mygtukas);
+    mygtukoNumeris++;
+}
+
+// =================================================== DVYLIKTAS
+
+function trintiElementa(elementoNumeris) {
+  var sarasoElementas = document.getElementById("sarasas");
+  var elementoId = "elementas" + elementoNumeris;
+  var elementas = document.getElementById(elementoId);
+
+  if (elementas) {
+      sarasoElementas.removeChild(elementas);
+  }
+}
+
+// ==================================================== Tryliktas
+
+function pakeistiAktyvuLangą(langasNumeris) {
+  var visiLangai = document.querySelectorAll('.langas');
+  for (var i = 0; i < visiLangai.length; i++) {
+      visiLangai[i].classList.remove('aktyvus');
+  }
+
+  var aktyvusLangas = document.getElementById('langas' + langasNumeris);
+  aktyvusLangas.classList.add('aktyvus');
+}
+
+//  ================================================= Keturioliktas
+
+var dabartinisPaveikslelis = 1;
+
+function keistiPaveiksli() {
+    dabartinisPaveikslelis++;
+    if (dabartinisPaveikslelis > 3) {
+        dabartinisPaveikslelis = 1;
+    }
+
+    var paveikslelis = document.getElementById("paveikslelis");
+    paveikslelis.src = "paveikslas" + dabartinisPaveikslelis + ".jpg";
+    paveikslelis.alt = "Paveikslėlis " + dabartinisPaveikslelis;
+}

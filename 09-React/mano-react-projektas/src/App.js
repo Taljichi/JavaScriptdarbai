c2dda5852ba6829b52d025860fdaf73ee8f6bee3
Marius Jankauskas <marius40555@gmail.com>
2023-12-18 19:45:53 +0200

    Veikia REACT

--- 09-React/mano-react-projektas/src/App.js
+++ 09-React/mano-react-projektas/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from '../components/navbar/Nav';
+import Header from '../components/header/Header';
+import Apiemus from '../components/apiemus/Apiemus';
+import Paslaugos from '../components/paslaugos/Paslaugos';
+import Footer from '../components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Apiemus />
+      <Paslaugos />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
